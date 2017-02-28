cc1b977623e9367b65399d19eda40607f7ab9697
Johan Niklasson <johan@nicklassons.se>
2017-02-28 15:37:51 +0100

    Added function to see if a player has won.

--- shared/board.c
+++ shared/board.c
@@ -43,13 +43,45 @@ void set_poisition(const char row, const char col, char value) {
     board_moves[row] |= value << (6 - col*2);
 }
 
+char player_has_won(void) {
+    int i;
+    for (i = 0; i < 2; i++) {
+        /* Check each column and row for a tree in a row*/
+        if (get_position(0, i) == get_position(1, i) &&
+            get_position(0, i) == get_position(2, i) &&
+            get_position(0, i) != 0) {
+            return get_position(0, i);
+        } else if (
+            get_position(i, 0) == get_position(i, 1) &&
+            get_position(i, 0) == get_position(i, 2) &&
+            get_position(i, 0) != 0) {
+            return get_position(i, 0);
+        }
+    }
+
+    /* Check diagonally */
+    if (get_position(0, 0) == get_position(1, 1) &&
+        get_position(0, 0) == get_position(2, 2) &&
+        get_position(0, 0) != 0) {
+        return get_position(0, 0);
+    } else if (
+        get_position(0, 2) == get_position(1, 1) &&
+        get_position(0, 2) == get_position(2, 0) &&
+        get_position(0, 2) != 0) {
+        return get_position(0, 2);
+    }
+
+    return 0;
+}
+
 /*
 int main() {
-    //board_moves[0] = 0b11000000;
-    //board_moves[1] = 0b00100000;
-    //board_moves[2] = 0b00001100;
+    set_poisition(0, 0, 2);
+    set_poisition(1, 0, 2);
+    set_poisition(2, 0, 2);
     set_poisition(1, 1, 1);
-    char pos = get_position(1, 1);
-    printf("Position 0, 0:\t %d\n", pos);
+    set_poisition(2, 2, 2);
+    char winning = player_has_won();
+    printf("Player has won:\t %d\n", winning);
 }
 */
