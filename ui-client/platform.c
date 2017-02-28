04e4a5416d3cc19568e7023b9e168e040784c653
Johan Niklasson <johan@nicklassons.se>
2017-02-28 16:47:10 +0100

    Added helper functions for setting and clearing port bits.

--- ui-client/platform.c
+++ ui-client/platform.c
@@ -5,6 +5,11 @@ char platform_get_any_button_state(char *row, char *col);
 void platform_set_led(const char row, const char col, const char state);
 void platform_set_all_led(const char state);
 
+void _PORTB(int state, int shift);
+void _PORTD(int state, int shift);
+void _PORTF(int state, int shift);
+void _PORTG(int state, int shift);
+
 /* Setup inputs & outputs etc */
 void platform_init(void) {
     // TRISESET = ... etc
@@ -58,9 +63,13 @@ char platform_get_any_button_state(char *row, char *col) {
 
 /* Lights up a LED in given state (0-2) */
 void platform_set_led(const char row, const char col, const char state) {
+    char b0 = state & 1;
+    char b1 = state & 2;
+
     if (row == 0) {
         if (col == 0) {
-            
+            _PORTD(b0, 8); // Example
+            _PORTD(b1, 9); // code
         } else if (col == 1) {
             
         } else if (col == 2) {
@@ -93,5 +102,39 @@ void platform_set_all_led(const char state) {
         for (j = 0; j < 3; j++) {
             platform_set_led(i, j, state);
         }
+    }
+}
+
+
+/* Helper functions to set and clear each port more easily */
+void _PORTB(int state, int shift) {
+    if (state == 1) {
+        PORTBCLR = (1 << shift);
+    } else {
+        PORTBSET = (1 << shift)
+    }
+}
+
+void _PORTD(int state, int shift) {
+    if (state == 1) {
+        PORTDCLR = (1 << shift);
+    } else {
+        PORTDSET = (1 << shift)
+    }
+}
+
+void _PORTF(int state, int shift) {
+    if (state == 1) {
+        PORTFCLR = (1 << shift);
+    } else {
+        PORTFSET = (1 << shift)
+    }
+}
+
+void _PORTG(int state, int shift) {
+    if (state == 1) {
+        PORTGCLR = (1 << shift);
+    } else {
+        PORTGSET = (1 << shift)
     }
 }
