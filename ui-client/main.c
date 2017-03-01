7b3e18ef72ae37b98c6e56709291c2196f20a64b
Johan Niklasson <johan@nicklassons.se>
2017-03-01 11:42:19 +0100

    Added read and poll functions to ui client.

--- ui-client/main.c
+++ ui-client/main.c
@@ -1,6 +1,12 @@
 #include "../shared/board.c"
 //#include "platform.c" (Will be included during compile)
 
+void game_reset(void);
+char poll_read_player_select(void);
+char poll_read_ai_select(void);
+void poll_read_valid_move(char *row, char *col);
+void poll_read_ai_move(char *row, char *col);
+
 #define FINAL_SCORE 3
 
 /* Using char instead of int since no
@@ -12,8 +18,10 @@ char player1_turn;  // If it's player 1 that's up to do next move
 char player_1_score;
 char player_2_score;
 
+char rounds_played;
+
 /* Resets the functions for a new game */
-void game_reset() {
+void game_reset(void) {
     board_reset();
     ai = 0;
 
@@ -21,40 +29,58 @@ void game_reset() {
 
     player_1_score = 0;
     player_2_score = 0;
+
+    rounds_played = 0;
 }
 
 int main() {
 
-    /* Half pseudo-function flow below */
-
     /* Loop forever to keep the game going */
+    char next_row = 0;
+    char next_col = 0;
     while (1) {
         game_reset();
 
         /* Stalling: Determine 1 or 2 players. If 1 player, set ai to 1 */
+        if (poll_read_player_select() == 1) {
+            ai = 1;
+        } else {
+            ai = 0;
+        }
 
         if (ai != 0) {
             /* Stalling: Determine AI level (easy, medium or hard). Sets ai to 1-3 */
+            ai = poll_read_ai_select();
         }
 
         while (player_1_score != FINAL_SCORE && player_2_score != FINAL_SCORE) {
             board_reset();
+            platform_set_all_led(0);
+            player1_turn = rounds_played % 2;
 
             while (board_moves_count < 9 && board_result_round == 0) {
                 if (player1_turn == 1) {
                     /* Stalling: Read input from player 1 */
+                    poll_read_valid_move(&next_row, &next_col);
+                    board_set_position(next_row, next_col, 1);
                 } else {
-                    if (ai > 0) {
-                        /* Send board and difficulty to ai-client */
-                        /* Ẃait: Recieve next move */
-                    } else {
+                    if (ai = 0) {
                         /* Stalling: Read input from player 2 */
+                        poll_read_valid_move(&next_row, &next_col);
+                    } else {
+                        /* Send board and difficulty to ai-client */
+                        /* Stalling: Recieve next move */
+                        poll_read_ai_move(&next_row, &next_col);
                     }
+                    board_set_position(next_row, next_col, 2);
                 }
-                board_moves_count++;
+                /* TODO: Print current state with LEDs */
+                board_moves_count = board_count_moves(0);
                 player1_turn = !player1_turn; // Sets player1_turn to 1 or 0 depending on last value
 
                 /* Do check if a player has three in a row , if so quit the loop */
+                board_result_round = board_player_has_won();
+                if (board_result_round != 0) break;
             }
 
             if (board_result_round != 0) {
@@ -63,14 +89,81 @@ int main() {
                 } else {
                     player_2_score++;
                 }
-                /* Flash the winning row a couple of times */
+                /* TODO: Flash the winning row a couple of times */
             } else {
                 player_1_score++;
                 player_2_score++;
-                /* Do a slight delay and maybe blink some lights to indicate the game is over */
             }
+            rounds_played++;
+            /* TODO: Wait a moment before starting the new match */
         }
+        /* TODO: Do a slight delay and maybe blink some lights to indicate the game is over */
     }
 
 	return 0;
+}
+
+char poll_read_player_select(void) {
+    /* Wait for a button to be pressed */
+    while (platform_get_button_state(0, 0) == 0 && 
+           platform_get_button_state(0, 2) == 0) {};
+
+    /* If 1 player was pressed */
+    if (platform_get_button_state(0, 0) == 1) {
+        /* Wait for its release and send player state */
+        while (platform_get_button_state(0, 0) == 1) {}
+        return 1;
+    }
+
+    /* If 2 player was pressed */
+    if (platform_get_button_state(0, 2) == 1) {
+        /* Wait for its release and send player state */
+        while (platform_get_button_state(0, 2) == 1) {}
+        return 2;
+    }
+}
+
+
+char poll_read_ai_select(void) {
+    /* Wait for a button to be pressed */
+    while (platform_get_button_state(0, 0) == 0 && 
+           platform_get_button_state(0, 1) == 0 && 
+           platform_get_button_state(0, 2) == 0) {};
+
+    /* If level 1 was pressed */
+    if (platform_get_button_state(0, 0) == 1) {
+        /* Wait for its release and send level state */
+        while (platform_get_button_state(0, 0) == 1) {}
+        return 1;
+    }
+
+    /* If level 2 was pressed */
+    if (platform_get_button_state(0, 1) == 1) {
+        /* Wait for its release and send level state */
+        while (platform_get_button_state(0, 1) == 1) {}
+        return 2;
+    }
+
+    /* If level 3 was pressed */
+    if (platform_get_button_state(0, 2) == 1) {
+        /* Wait for its release and send level state */
+        while (platform_get_button_state(0, 2) == 1) {}
+        return 3;
+    }
+}
+
+void poll_read_valid_move(char *row, char *col) {
+    while (1) {
+        if (platform_get_any_button_state(row, col)) {
+            if (board_get_position(*row, *col) == 0) {
+                while (platform_get_button_state(row, col) == 1) {}
+                return;
+            }
+        }
+    }
+}
+
+void poll_read_ai_move(char *row, char *col) {
+    while (1) {}
+    // TODO: Make a nice protocol here
 }
